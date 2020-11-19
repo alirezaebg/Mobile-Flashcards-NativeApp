ce78ea50b157bdc17c15918268f3e12558c1e805
Alireza Ebadighajari <alirezaebg@gmail.com>
2020-11-18 23:45:00 -0800

    Add TabNavigator

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-// import 'react-native-gesture-handler'
 import React from 'react'
-import { View, Platform, StatusBar } from 'react-native'
-import Deck from './components/Decks'
-import { purple } from './utils/colors'
+import { View, Text, Platform, StatusBar } from 'react-native'
+import Decks from './components/Decks'
 import NewDeck from './components/NewDeck'
 import Constants from 'expo-constants'
+import { purple, white, lightPurp } from './utils/colors'
 
-// import { NavigationContainer } from "@react-navigation/native"
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 export default function App() {
 
@@ -19,51 +19,61 @@ export default function App() {
     )
   }
 
-  // const RouteConfigs = {
-  //   Decks: {
-  //     name: "Decks",
-  //     component: Decks,
-  //   },
-  //   NewDeck: {
-  //     name: "NewDeck",
-  //     component: NewDeck,
-  //   }
-  // }
+  const RouteConfigs = {
+    Decks: {
+      name: "Decks",
+      component: Decks,
+      options: { 
+        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="cards" size={30} color={color} />,
+        tabBarLabel: () => <Text style={{color: purple, fontSize: 12}}>Decks</Text>
+      }
+    },
+    NewDeck: {
+      name: "NewDeck",
+      component: NewDeck,
+      options: { 
+        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="cards-spade" size={30} color={color} />,
+        tabBarLabel: () => <Text style={{color: purple, fontSize: 12}}>New Deck</Text>
+      }
+    }
+  }
 
-  // const TabNavigatorConfig = {
-  //   navigationOptions: {
-  //     header: null
-  //   },
-  //   tabBarOptions: {
-  //     activeTintColor: Platform.OS === "ios" ? purple : white,
-  //     style: {
-  //       height: 56,
-  //       backgroundColor: Platform.OS === "ios" ? white : purple,
-  //       shadowColor: "rgba(0, 0, 0, 0.24)",
-  //       shadowOffset: {
-  //         width: 0,
-  //         height: 3
-  //       },
-  //       shadowRadius: 6,
-  //       shadowOpacity: 1
-  //     }
-  //   }
-  // }
+  const TabNavigatorConfig = {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? lightPurp : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : lightPurp,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  }
 
-  // const Tab = createBottomTabNavigator()
+  const Tab = createBottomTabNavigator()
 
-  // const TabNav = () => (
-  //   <Tab.Navigator {...TabNavigatorConfig}>
-  //     <Tab.Screen {...RouteConfigs['Decks']} />
-  //     <Tab.Screen {...RouteConfigs['NewDeck']} />
-  //   </Tab.Navigator>
-  // )
+  const TabNav = () => (
+    <Tab.Navigator {...TabNavigatorConfig}>
+      <Tab.Screen {...RouteConfigs['Decks']} />
+      <Tab.Screen {...RouteConfigs['NewDeck']} />
+    </Tab.Navigator>
+  )
 
 
   return (
     <View style={{ flex: 1 }}>
       <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
-      <Deck />
+      <NavigationContainer >
+        <TabNav />
+      </NavigationContainer>
     </View>
   )
 }
